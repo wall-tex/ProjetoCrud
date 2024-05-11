d61f7ef893cf9c2237a75497186c587d975c2578
wall-tex <149019685+wall-tex@users.noreply.github.com>
2024-05-10 23:42:18 -0300

    Add files via upload

---
+++ PythonProjetos/setup/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from todos.views import todo_list
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", todo_list),
+]
---
+++ PythonProjetos/todos/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from .models import Todo
+
+
+def todo_list(request):
+    todos = Todo.objects.all()
+
+    return render(request, "todos/todo_list.html", {"todos": todos})
